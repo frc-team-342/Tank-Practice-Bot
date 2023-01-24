648a96518cc5448c452f49aa21c57e1e3ed70852
Jamair <jammyque@gmail.com>
2023-01-24 18:18:28 -0500

    updated auto timed drive values

--- src/main/java/frc/robot/subsystems/DriveSystem.java
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DriveSystem extends SubsystemBase {
 
   private AHRS navX;
-  
+
   private WPI_TalonSRX motorLeftOne;
   private WPI_TalonSRX motorLeftTwo;
   private MotorControllerGroup leftMotorGroup;
